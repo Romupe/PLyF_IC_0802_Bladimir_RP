e15ac540c53508797f33a6549c0fe805ebe96df2
Romupe <102791768+Romupe@users.noreply.github.com>
2022-07-05 03:19:24 -0500

    Create Principal.py

---
+++ Corrección_examen/Principal.py
@@ -0,0 +1,9 @@
+class PruebaExamen:
+    def mensajeHola(self):
+        print("Hola mi nombre es: Fernando Mercado")
+    def mensajeHola(Self, cadena):
+        print("La cadena es: "+ cadena)
+    def operacionPrueba(a,b):
+        return (a+b)
+    def operacionPrueba(self, a,b,c):
+        pass
